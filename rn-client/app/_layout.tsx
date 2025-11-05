084c9b5939f7b7b6c13f03e2124e3176ed8d2703
nabilfikrisp <mnabilfikrisp@gmail.com>
2025-11-05 10:23:48 +0700

    feat: barebone expo

--- rn-client/app/_layout.tsx
+++ rn-client/app/_layout.tsx
@@ -1,24 +1,10 @@
 import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import "react-native-reanimated";
-
-import Providers from "@/components/providers";
-
-export const unstable_settings = {
-  anchor: "(tabs)",
-};
 
 export default function RootLayout() {
   return (
-    <Providers>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="modal"
-          options={{ presentation: "modal", title: "Modal" }}
-        />
-      </Stack>
-      <StatusBar style="auto" />
-    </Providers>
+    <Stack>
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen name="login" options={{ title: "Login" }} />
+    </Stack>
   );
 }
